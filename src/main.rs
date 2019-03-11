ae4465233c5e38bfac4a6feaa44f54fd9b37267c
Chris Schnabel <chrisjschnabel@gmail.com>
2019-03-10 23:57:00 -0400

    removed rocket (for now at least).

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,21 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-
-// #[macro_use] extern crate rocket;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 use std::process::Command;
-//use serde::{Deserialize, Serialize};
 use serde_json::Result;
 
 mod types;
 
-//#[get("/")]
-//fn index() -> &'static str {
-//    "Hello, world!"
-//}
 
 fn main() {
-    // rocket::ignite().mount("/", routes![index]).launch();
-
     let result: Result<Vec<types::Channel>> = list_channels();
     let channels = result.unwrap();
     println!("{:#?}", channels[0]);
     println!("{:#?}", get_wallet_balance().unwrap());
 }
+
 
 fn lncli(command: String) -> String {
     let output = Command::new("lncli")
